649f056101b1b98e62af3e18aa92c3c8d1468995
Dmitry Lyuchkov <d.lyuchkov@gmail.com>
2026-02-18 12:54:40 +0300

    getAll history addition fix

--- src/main/java/tracker/managers/InMemoryTaskManager.java
+++ src/main/java/tracker/managers/InMemoryTaskManager.java
@@ -73,23 +73,17 @@ public class InMemoryTaskManager implements TaskManager {
 
     @Override
     public ArrayList<Subtask> getAllSubtasks() {
-        ArrayList<Subtask> subtasks = new ArrayList<>(this.subtasks.values());
-        subtasks.forEach(historyManager::add);
-        return subtasks;
+        return new ArrayList<>(this.subtasks.values());
     }
 
     @Override
     public ArrayList<Epic> getAllEpics() {
-        ArrayList<Epic> epics = new ArrayList<>(this.epics.values());
-        epics.forEach(historyManager::add);
-        return epics;
+        return new ArrayList<>(this.epics.values());
     }
 
     @Override
     public ArrayList<Task> getAllTasks() {
-        ArrayList<Task> tasks = new ArrayList<>(this.tasks.values());
-        tasks.forEach(historyManager::add);
-        return tasks;
+        return new ArrayList<>(this.tasks.values());
     }
 
     @Override
